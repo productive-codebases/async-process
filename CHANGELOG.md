a47c7e5f21d2e9ec3df23396a7ed5acb9ff84e20
Alexis Mineaud <amineaud@tenable.com>
2023-01-06 20:02:24 +0100

    Expose withLogs and olderThan functions.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## v1.1.0
+
+### Added
+
+- Expose `withLogs` composer.
+- Expose `olderThan` predicate.
+
 ## v1.0.0
 
 ### Added
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { AsyncProcess } from './AsyncProcess'
 
+export { withLogs } from './AsyncProcess/composers/withLogs'
+export { olderThan } from './AsyncProcess/predicates/olderThan'
+
 export * from './types'
